7d7da1ee9687c653a48ab9ed9cd736ca4928a8b1
Pawel Pasich <pawelpasich@gmail.com>
2023-02-20 22:29:12 +0000

    Namespace rename

--- tests/Unit/App/Services/CarServiceTest.php
+++ tests/Unit/App/Services/CarServiceTest.php
@@ -1,27 +1,27 @@
 <?php
 
-namespace Tests\Unit\AppBundle\Services;
+namespace Unit\AppBundle\Services;
 
-use App\Services\CarService;
+use Foxmark\Services\CarService;
 use PHPUnit\Framework\TestCase;
 
 class CarServiceTest extends TestCase
 {
-    public function testOne()
+    public function testTrueIsAlwaysTrue()
     {
         $this->assertTrue(true);
     }
 
-    public function testTwo()
+    public function testCarIsInstanceOfCarService()
     {
         $car = new CarService();
         $this->assertInstanceOf(CarService::class, $car);
     }
 
-    public function testThree()
+    public function testCarServiceConf1IsAlways23()
     {
         $car = new CarService();
         $car_config = $car::CONF_1;
         $this->assertEquals(23, $car_config);
-}
+    }
 }
